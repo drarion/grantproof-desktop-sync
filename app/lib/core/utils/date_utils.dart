788b8d215d7ab57db77448e499ccc5b199da7680
drarion <christian.noirard@gmail.com>
2026-04-11 08:20:41 +0200

    Delete app directory

--- app/lib/core/utils/date_utils.dart
+++
@@ -1,9 +0,0 @@
-import 'package:intl/intl.dart';
-
-class DateUtilsX {
-  static final DateFormat short = DateFormat('dd/MM/yyyy');
-  static final DateFormat monthLabel = DateFormat('MM/yyyy');
-
-  static String formatShort(DateTime value) => short.format(value);
-  static String formatMonth(DateTime value) => monthLabel.format(value);
-}
